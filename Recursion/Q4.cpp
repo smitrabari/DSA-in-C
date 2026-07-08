db1dec367e8cdadb7103d0764e4ac7f940c5d9dc
smitrabari <smitrabari18@gmail.com>
2026-07-08 19:28:15 +0530

    Pirnt Linearly from N to 1!!

---
+++ Recursion/Q4.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Pirnt Linearly from 1 to N
+
+void Linearly(int i, int n)
+{
+	if(i<1)
+	{
+		return;
+	}
+	Linearly(i-1,n);
+	cout<<i<<endl;
+}
+
+int main()
+{
+	int n = 10;
+	Linearly(n , n);
+	return 0;
+}
