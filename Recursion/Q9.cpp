3565851a76d1acaa34d2f6bf5c728bf52691aceb
smitrabari <smitrabari18@gmail.com>
2026-07-09 23:56:18 +0530

    Trails

---
+++ Recursion/Q9.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Reverse an Array!!
+
+int Fact(int n)
+{
+	if(n==1)
+	{
+		return 0;
+	}
+	return n * Fact(n-1);
+}
+
+int main()
+{
+	int arr[n] = {};
+	int n = 5;
+	Fact(n);
+	return 0;
+}
