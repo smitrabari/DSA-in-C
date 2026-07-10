ed451f8fca752545762851ab4b8d012ab288e8df
smitrabari <smitrabari18@gmail.com>
2026-07-10 22:16:25 +0530

    Reverse an Array using 2 pointers!!

--- Recursion/Q9.cpp
+++ Recursion/Q9.cpp
@@ -1,21 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Reverse an Array!!
+//Reverse an Array using 2 pointers!!
 
-int rev(int i,int n)
+void rev(int i,int n,int arr[])
 {
-	if(i>=n)
+	if(i >= n/2)
 	{
-		return
+		return ;
 	}
-	return rev(i+1,n-1);
+	swap(arr[i],arr[n-i-1]);
+	rev(i+1,n,arr);
 }
 
 int main()
 {
 	int n = 5;
-	int arr[n] = {1,3,4,5,2,5,4};
-	rev(0,n);
+	int arr[n] = {1,3,4,5,2};
+	rev(0,n,arr);
+	for(int j = 0;j < n;j++)
+	{
+		cout<<arr[j]<<" ";
+	}
 	return 0;
 }
